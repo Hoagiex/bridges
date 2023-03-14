ed58ff5256cfffae2030ad080d5f76791dc4fa95
emielklomp <sDDBVwe2Arpgw8dhP395>
2023-03-14 22:16:34 +0100

    code clean up

--- src/main/kotlin/practice/dto/BrugopeningConflictDto.kt
+++ src/main/kotlin/practice/dto/BrugopeningConflictDto.kt
@@ -4,9 +4,4 @@ data class BrugopeningConflictDto(
     val brugnaam: String,
     val eerste: BrugopeningDto,
     val tweede: BrugopeningDto
-) {
-
-    override fun toString(): String {
-        return "BrugopeningConflictDto(brugnaam='$brugnaam', eerste=$eerste, tweede=$tweede)"
-    }
-}
+)
--- src/main/kotlin/practice/dto/BrugopeningDto.kt
+++ src/main/kotlin/practice/dto/BrugopeningDto.kt
@@ -6,9 +6,4 @@ data class BrugopeningDto(
     val brugnaam: String,
     val openingsTijd: LocalTime,
     val sluitingsTijd: LocalTime
-) {
-
-    override fun toString(): String {
-        return "BrugopeningDto(brugnaam='$brugnaam', openingsTijd=$openingsTijd, sluitingsTijd=$sluitingsTijd)"
-    }
-}
+)
